228bf2019097629e48ac875cd08a81f788d83764
Ira <sokurira@gmail.com>
2023-10-06 17:00:43 +0300

    sum of negative numbers

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+while True:
+    try:
+        NUM_SIZE = int(input("Enter size of list: "))
+        break
+    except:
+        print("Enter only integer numbers please!")
+
+
+numbers = []
+for num in range(NUM_SIZE):
+    numbers.append(random.randint(-100, 100))
+
+print(numbers)
+sum_negative = 0
+
+#1
+for num in numbers:
+    if num < 0:
+        sum_negative += num
+    else:
+        continue
+
+print(f"Sum of negative numbers: {sum_negative}")
+
